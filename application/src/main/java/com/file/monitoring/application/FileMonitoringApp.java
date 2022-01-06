4acbc12841831d0e0b76ae050a807e014596bc85
Sriram Ponangi <19464979+sriram-ponangi@users.noreply.github.com>
2022-01-06 18:29:11 -0400

    Adding a shutdown hook

--- application/src/main/java/com/file/monitoring/application/FileMonitoringApp.java
+++ application/src/main/java/com/file/monitoring/application/FileMonitoringApp.java
@@ -2,6 +2,7 @@ package com.file.monitoring.application;
 
 import com.file.monitoring.application.events.MonitoringEventsLoader;
 import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,8 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.annotation.PreDestroy;
 
 @SpringBootApplication(scanBasePackages = {"com.file.monitoring"})
 @EnableEncryptableProperties
@@ -19,7 +22,7 @@ public class FileMonitoringApp implements CommandLineRunner {
     @Qualifier("MonitoringEventsLoader")
     private MonitoringEventsLoader monitoringEventsLoader;
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         SpringApplication.run(FileMonitoringApp.class, args);
     }
@@ -32,8 +35,27 @@ public class FileMonitoringApp implements CommandLineRunner {
             LOGGER.info("Registered Event Monitors = {} ", MonitoringEventsLoader.REGISTERED_EVENT_MONITORS);
 
         } catch (Exception e) {
-            LOGGER.error("{} ", e);
+            LOGGER.error("Application Could Not be Started Properly. {} ", ExceptionUtils.getRootCauseMessage(e));
         }
+    }
+    
+    @PreDestroy
+    public void onExit() {
+        LOGGER.info("STOPPING THE ACTIVE FILE MONITORS. TOTAL ACTIVE FILE MONITORS = {} ", MonitoringEventsLoader.REGISTERED_EVENT_MONITORS.size());
+        try {
+
+            MonitoringEventsLoader.REGISTERED_EVENT_MONITORS.forEach((key, fileAlterationMonitor) -> {
+                try {
+                    LOGGER.info("Stopping File Monitor = {} ", key);
+                    fileAlterationMonitor.stop();
+                } catch (Exception e) {
+                    LOGGER.error("Failed To Stop File Monitor For {} With Error: {} ", key, ExceptionUtils.getRootCauseMessage(e));
+                }
+            });
+        } catch (Exception e) {
+            LOGGER.error("Application Shutdown Unsuccessful. {} ", ExceptionUtils.getRootCauseMessage(e));
+        }
+        LOGGER.info("ALL ACTIVE FILE MONITORS HAVE BEEN STOPPED.");
     }
 
 
